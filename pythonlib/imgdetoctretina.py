318d5f5c13f26a476a051b991720d58113b81cdb
sujatha-sureshkmr <sujatha.sureshkmr@gmail.com>
2024-05-06 03:23:14 +0800

    commit

--- pythonlib/imgdetoctretina.py
+++ pythonlib/imgdetoctretina.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdb398a3ef2001ebb9faf6d1792e7130f402c62ec9b83010f18a353f5f523c6a
-size 8646
+oid sha256:2a137dbc077ab1d0a86b6e0feadb782f2f6dcd627ead253ba2c8d37d66fc6669
+size 8844
